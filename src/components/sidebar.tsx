a6d1fc519acb5792658a628435c0b7c9a0efca5e
Lior Pendler <lior.pendler@gmail.com>
2024-08-02 13:46:39 -0700

    Try and rework combobox.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Button } from './ui/button';
 import {
 	Card,
@@ -7,14 +8,28 @@ import {
 	CardHeader,
 	CardTitle,
 } from './ui/card';
+import { Combobox } from './ui/create-select';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 
 export default function Sidebar() {
+	const [selected, setSelected] = useState<string>('');
+	// const [tasks, setTasks] = useState<Task[]>([
+	//     {
+
+	//     }
+	// ]);
+	const [tasks, setTasks] = useState<{ name: string }[]>([
+		{ name: 'AAA' },
+		{ name: 'BBB' },
+		{ name: 'CCC' },
+		{ name: 'DDD' },
+		{ name: 'EEE' },
+	]);
 	return (
 		<Tabs
-			className="flex w-full flex-col items-center justify-center"
+			className="flex w-full flex-col items-center justify-center pl-2"
 			defaultValue="user">
 			<TabsList className="w-[80%]">
 				<TabsTrigger value="user" className="w-full">
@@ -25,12 +40,12 @@ export default function Sidebar() {
 				</TabsTrigger>
 			</TabsList>
 			<TabsContent value="user" className="w-full">
-				<Card side={'left'}>
-					<CardHeader>
+				<Card side={'left'} className="pt-6">
+					{/* <CardHeader>
 						<CardTitle>User</CardTitle>
 						<CardDescription></CardDescription>
-					</CardHeader>
-					<CardContent className="">
+					</CardHeader> */}
+					<CardContent>
 						<Label htmlFor="name">Name</Label>
 						<Input defaultValue={'Name'} id="name" className="mb-6" />
 						<Label htmlFor="pfp">Profile Picture</Label>
@@ -45,13 +60,41 @@ export default function Sidebar() {
 				</Card>
 			</TabsContent>
 			<TabsContent value="task" className="w-full">
-				<Card side={'left'}>
-					<CardHeader>
-						<CardTitle>Tasks</CardTitle>
+				<Card side={'left'} className="pb-3 pt-3">
+					{/* <CardHeader>
+						// <CardTitle>Tasks</CardTitle>
 						<CardDescription></CardDescription>
-					</CardHeader>
-					<CardContent></CardContent>
-					<CardFooter></CardFooter>
+					</CardHeader> */}
+					<CardContent>
+						{/* <Tabs defaultValue="edit">
+							<TabsList className="w-full">
+								<TabsTrigger value="edit" className="w-full">
+									Edit
+								</TabsTrigger>
+								<TabsTrigger value="create" className="w-full">
+									Create
+								</TabsTrigger>
+							</TabsList>
+							<TabsContent value="edit">
+								<h1>Editing Task</h1>
+							</TabsContent>
+							<TabsContent value="create">
+								<h1>Create New Task</h1>
+							</TabsContent>
+						</Tabs> */}
+					</CardContent>
+					<Combobox
+						options={tasks}
+						placeholder="Select option..."
+						selected={selected}
+						onChange={setSelected}
+					/>
+					<CardFooter>
+						{/* TODO: Make it popup with a thing saying "saving" */}
+						<Button variant={'outline'} className="ml-auto">
+							Save
+						</Button>
+					</CardFooter>
 				</Card>
 			</TabsContent>
 		</Tabs>
