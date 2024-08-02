cba1c790c5bc0b71b5968d744476a2bd0e628ef9
Lior Pendler <lior.pendler@gmail.com>
2024-08-02 12:33:57 -0700

    Add shadcn component and start with sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import { Button } from './ui/button';
 import {
 	Card,
 	CardContent,
@@ -6,16 +7,53 @@ import {
 	CardHeader,
 	CardTitle,
 } from './ui/card';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 
 export default function Sidebar() {
 	return (
-		<Card side={'left'}>
-			<CardHeader>
-				<CardTitle></CardTitle>
-				<CardDescription></CardDescription>
-			</CardHeader>
-			<CardContent></CardContent>
-			<CardFooter></CardFooter>
-		</Card>
+		<Tabs
+			className="flex w-full flex-col items-center justify-center"
+			defaultValue="user">
+			<TabsList className="w-[80%]">
+				<TabsTrigger value="user" className="w-full">
+					User
+				</TabsTrigger>
+				<TabsTrigger value="task" className="w-full">
+					Task
+				</TabsTrigger>
+			</TabsList>
+			<TabsContent value="user" className="w-full">
+				<Card side={'left'}>
+					<CardHeader>
+						<CardTitle>User</CardTitle>
+						<CardDescription></CardDescription>
+					</CardHeader>
+					<CardContent className="">
+						<Label htmlFor="name">Name</Label>
+						<Input defaultValue={'Name'} id="name" className="mb-6" />
+						<Label htmlFor="pfp">Profile Picture</Label>
+						<Input id="pfp" type="file" />
+					</CardContent>
+					<CardFooter>
+						{/* TODO: Make it popup with a thing saying "saving" */}
+						<Button variant={'outline'} className="ml-auto">
+							Save
+						</Button>
+					</CardFooter>
+				</Card>
+			</TabsContent>
+			<TabsContent value="task" className="w-full">
+				<Card side={'left'}>
+					<CardHeader>
+						<CardTitle>Tasks</CardTitle>
+						<CardDescription></CardDescription>
+					</CardHeader>
+					<CardContent></CardContent>
+					<CardFooter></CardFooter>
+				</Card>
+			</TabsContent>
+		</Tabs>
 	);
 }
