4ca7c397ab7140550f70ed0a5d1f9faafcc0c28b
underscorelior <newlioryt@gmail.com>
2024-08-05 12:04:13 -0700

    More work at table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import * as React from 'react';
 import {
 	ColumnDef,
 	ColumnFiltersState,
@@ -16,7 +15,6 @@ import {
 import { ArrowUpDown, ChevronDown, MoreHorizontal } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
 import {
 	DropdownMenu,
 	DropdownMenuCheckboxItem,
@@ -35,8 +33,79 @@ import {
 	TableHeader,
 	TableRow,
 } from '@/components/ui/table';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from './ui/card';
+
+import { useWindowSize } from '@uidotdev/usehooks';
+import { useState } from 'react';
 
 const data: Payment[] = [
+	{
+		id: 'm5gr84i9',
+		amount: 316,
+		status: 'success',
+		email: 'ken99@yahoo.com',
+	},
+	{
+		id: '3u1reuv4',
+		amount: 242,
+		status: 'success',
+		email: 'Abe45@gmail.com',
+	},
+	{
+		id: 'derv1ws0',
+		amount: 837,
+		status: 'processing',
+		email: 'Monserrat44@gmail.com',
+	},
+	{
+		id: '5kma53ae',
+		amount: 874,
+		status: 'success',
+		email: 'Silas22@gmail.com',
+	},
+	{
+		id: 'bhqecj4p',
+		amount: 721,
+		status: 'failed',
+		email: 'carmella@hotmail.com',
+	},
+	{
+		id: 'm5gr84i9',
+		amount: 316,
+		status: 'success',
+		email: 'ken99@yahoo.com',
+	},
+	{
+		id: '3u1reuv4',
+		amount: 242,
+		status: 'success',
+		email: 'Abe45@gmail.com',
+	},
+	{
+		id: 'derv1ws0',
+		amount: 837,
+		status: 'processing',
+		email: 'Monserrat44@gmail.com',
+	},
+	{
+		id: '5kma53ae',
+		amount: 874,
+		status: 'success',
+		email: 'Silas22@gmail.com',
+	},
+	{
+		id: 'bhqecj4p',
+		amount: 721,
+		status: 'failed',
+		email: 'carmella@hotmail.com',
+	},
 	{
 		id: 'm5gr84i9',
 		amount: 316,
@@ -172,13 +241,11 @@ export const columns: ColumnDef<Payment>[] = [
 ];
 
 export function TaskTable() {
-	const [sorting, setSorting] = React.useState<SortingState>([]);
-	const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-		[],
-	);
-	const [columnVisibility, setColumnVisibility] =
-		React.useState<VisibilityState>({});
-	const [rowSelection, setRowSelection] = React.useState({});
+	const size = useWindowSize();
+	const [sorting, setSorting] = useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+	const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+	const [rowSelection, setRowSelection] = useState({});
 
 	const table = useReactTable({
 		data,
@@ -197,115 +264,127 @@ export function TaskTable() {
 			columnVisibility,
 			rowSelection,
 		},
+		initialState: {
+			pagination: {
+				pageSize: Math.round(
+					((size.height || window.innerHeight) * 0.8 - 186) / 60,
+				),
+				pageIndex: 0,
+			},
+		},
 	});
 
 	return (
-		<div className="w-full">
-			<div className="flex items-center py-4">
-				<Input
-					placeholder="Filter tasks..."
-					value={(table.getColumn('task')?.getFilterValue() as string) ?? ''}
-					onChange={(event) =>
-						table.getColumn('task')?.setFilterValue(event.target.value)
-					}
-					className="max-w-sm"
-				/>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant="outline" className="ml-auto">
-							Columns <ChevronDown className="ml-2 h-4 w-4" />
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						{table
-							.getAllColumns()
-							.filter((column) => column.getCanHide())
-							.map((column) => {
-								return (
-									<DropdownMenuCheckboxItem
-										key={column.id}
-										className="capitalize"
-										checked={column.getIsVisible()}
-										onCheckedChange={(value) =>
-											column.toggleVisibility(!!value)
-										}>
-										{column.id}
-									</DropdownMenuCheckboxItem>
-								);
-							})}
-					</DropdownMenuContent>
-				</DropdownMenu>
-			</div>
-			<div className="rounded-md border">
-				<Table>
-					<TableHeader>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map((header) => {
+		<Card className="flex h-[80vh] flex-col">
+			<CardHeader>
+				<CardTitle>
+					Verify {size.height} {window.innerHeight}{' '}
+					{Math.round(((size.height || window.innerHeight) * 0.8 - 186) / 60)}{' '}
+					{((size.height || window.innerHeight) * 0.8 - 186) / 60}
+				</CardTitle>
+				<CardDescription></CardDescription>
+			</CardHeader>
+			<CardContent className="flex flex-col justify-start">
+				<div className="flex items-center py-4">
+					<Input
+						placeholder="Filter tasks..."
+						value={(table.getColumn('task')?.getFilterValue() as string) ?? ''}
+						onChange={(event) =>
+							table.getColumn('task')?.setFilterValue(event.target.value)
+						}
+						className="max-w-sm"
+					/>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button variant="outline" className="ml-auto">
+								Columns <ChevronDown className="ml-2 h-4 w-4" />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align="end">
+							{table
+								.getAllColumns()
+								.filter((column) => column.getCanHide())
+								.map((column) => {
 									return (
-										<TableHead key={header.id}>
-											{header.isPlaceholder
-												? null
-												: flexRender(
-														header.column.columnDef.header,
-														header.getContext(),
-													)}
-										</TableHead>
+										<DropdownMenuCheckboxItem
+											key={column.id}
+											className="capitalize"
+											checked={column.getIsVisible()}
+											onCheckedChange={(value) =>
+												column.toggleVisibility(!!value)
+											}>
+											{column.id}
+										</DropdownMenuCheckboxItem>
 									);
 								})}
-							</TableRow>
-						))}
-					</TableHeader>
-					<TableBody>
-						{table.getRowModel().rows?.length ? (
-							table.getRowModel().rows.map((row) => (
-								<TableRow
-									key={row.id}
-									data-state={row.getIsSelected() && 'selected'}>
-									{row.getVisibleCells().map((cell) => (
-										<TableCell key={cell.id}>
-											{flexRender(
-												cell.column.columnDef.cell,
-												cell.getContext(),
-											)}
-										</TableCell>
-									))}
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+				<div className="rounded-md border">
+					<Table>
+						<TableHeader>
+							{table.getHeaderGroups().map((headerGroup) => (
+								<TableRow key={headerGroup.id}>
+									{headerGroup.headers.map((header) => {
+										return (
+											<TableHead key={header.id}>
+												{header.isPlaceholder
+													? null
+													: flexRender(
+															header.column.columnDef.header,
+															header.getContext(),
+														)}
+											</TableHead>
+										);
+									})}
 								</TableRow>
-							))
-						) : (
-							<TableRow>
-								<TableCell
-									colSpan={columns.length}
-									className="h-24 text-center">
-									No results.
-								</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</div>
-			<div className="flex items-center justify-end space-x-2 py-4">
-				<div className="text-muted-foreground flex-1 text-sm">
-					{table.getFilteredSelectedRowModel().rows.length} of{' '}
-					{table.getFilteredRowModel().rows.length} row(s) selected.
+							))}
+						</TableHeader>
+						<TableBody>
+							{table.getRowModel().rows?.length ? (
+								table.getRowModel().rows.map((row) => (
+									<TableRow
+										key={row.id}
+										data-state={row.getIsSelected() && 'selected'}>
+										{row.getVisibleCells().map((cell) => (
+											<TableCell key={cell.id}>
+												{flexRender(
+													cell.column.columnDef.cell,
+													cell.getContext(),
+												)}
+											</TableCell>
+										))}
+									</TableRow>
+								))
+							) : (
+								<TableRow>
+									<TableCell
+										colSpan={columns.length}
+										className="h-24 text-center">
+										No results.
+									</TableCell>
+								</TableRow>
+							)}
+						</TableBody>
+					</Table>
 				</div>
-				<div className="space-x-2">
-					<Button
-						variant="outline"
-						size="sm"
-						onClick={() => table.previousPage()}
-						disabled={!table.getCanPreviousPage()}>
-						Previous
-					</Button>
-					<Button
-						variant="outline"
-						size="sm"
-						onClick={() => table.nextPage()}
-						disabled={!table.getCanNextPage()}>
-						Next
-					</Button>
-				</div>
-			</div>
-		</div>
+			</CardContent>
+			<CardFooter className="mt-auto flex w-full items-center justify-end space-x-2 pb-6">
+				<Button
+					variant="outline"
+					size="sm"
+					onClick={() => table.previousPage()}
+					disabled={!table.getCanPreviousPage()}>
+					Previous
+				</Button>
+				<Button
+					variant="outline"
+					size="sm"
+					onClick={() => table.nextPage()}
+					disabled={!table.getCanNextPage()}>
+					Next
+				</Button>
+			</CardFooter>
+		</Card>
 	);
 }
