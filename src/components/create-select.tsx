356975695db14994c92fe6be166cc4ac9c19d8c8
underscorelior <newlioryt@gmail.com>
2024-08-11 18:56:04 -0700

    Complete creating the shadcn form

--- src/components/create-select.tsx
+++ src/components/create-select.tsx
@@ -20,7 +20,6 @@ import {
 	DialogTrigger,
 } from './ui/dialog';
 import { Input } from './ui/input';
-import { Label } from './ui/label';
 import {
 	Select,
 	SelectContent,
@@ -79,10 +78,12 @@ const formSchema = z.object({
 		.min(1, { message: 'Description must be a minimum of 1 character.' }),
 	points: z
 		.number()
-		.min(0.1, { message: 'Task must be worth a minimum of 0.1 points.' }),
-	category: z.enum(['health', 'normal', 'cool', 'productivity', 'insane', '']),
-	type: z.enum(['daily', 'multi', 'single', 'weekly', '']),
-	lower: z.enum(['higher', 'lower']),
+		.gt(0, { message: 'Task must be worth more than 0 points.' }),
+	// category: z.enum(['health', 'normal', 'cool', 'productivity', 'insane', '']),
+	// type: z.enum(['daily', 'multi', 'single', 'weekly', '']),
+	category: z.string({ required_error: 'You must specify a category.' }),
+	type: z.string({ required_error: 'You must specify a type.' }),
+	lower: z.enum(['higher', 'lower']), // FIXME: try and make this a boolean
 });
 
 function TaskDialog({
@@ -96,19 +97,21 @@ function TaskDialog({
 }) {
 	const [out, setOut] = useState<Task>(genInitialOut());
 	const [open, setOpen] = useState<boolean>(false);
-	const [name, setName] = useState<string>('');
-	const [type, setType] = useState<string>('');
-	const [prio, setPrio] = useState<boolean>(false);
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			name: '',
-			description: '',
-			points: 0,
-			category: '',
-			type: '',
-			lower: 'higher',
+			name: task?.name || '',
+			description: task?.description || '',
+			points: task?.points || 0,
+			category: task?.category || '',
+			type: task?.type || '',
+			lower:
+				task?.lower !== undefined
+					? task.lower
+						? 'lower'
+						: 'higher'
+					: 'higher',
 		},
 	});
 
@@ -220,8 +223,6 @@ function TaskDialog({
 	}
 
 	function onSubmit(values: z.infer<typeof formSchema>) {
-		// Do something with the form values.
-		// ✅ This will be type-safe and validated.
 		console.log(values);
 	}
 
@@ -233,7 +234,17 @@ function TaskDialog({
 						<RiEditLine />
 					</div>
 				) : (
-					<div className="text-primary m-0 flex h-min flex-row items-center justify-center gap-2 whitespace-nowrap rounded-md p-0 text-sm font-medium underline-offset-4 transition-colors hover:underline disabled:pointer-events-none disabled:opacity-50">
+					<div
+						onClick={() => {
+							form.setValue('category', '');
+							form.setValue('description', '');
+							form.setValue('lower', 'higher');
+							form.setValue('name', '');
+							form.setValue('points', 0);
+							form.setValue('type', '');
+							form.clearErrors();
+						}}
+						className="text-primary m-0 flex h-min flex-row items-center justify-center gap-2 whitespace-nowrap rounded-md p-0 text-sm font-medium underline-offset-4 transition-colors hover:underline disabled:pointer-events-none disabled:opacity-50">
 						<IoCreateOutline /> Create new task
 					</div>
 				)}
@@ -261,19 +272,17 @@ function TaskDialog({
 							control={form.control}
 							render={({ field }) => (
 								<FormItem>
-									<FormLabel className="mb-1 text-base font-medium">
-										Name
-									</FormLabel>
-									<FormControl>
+									<FormLabel className="text-base font-medium">Name</FormLabel>
+									<FormControl className="!mt-0.5">
 										<Input
-											// defaultValue={out.name}
+											defaultValue={field.value}
 											placeholder="Task Name"
 											className="mb-4"
-											// maxLength={64}
+											maxLength={64}
 											{...field}
 										/>
 									</FormControl>
-									<FormDescription className="-mt-3 mb-2 font-mono text-xs font-normal">
+									<FormDescription className="!mt-0.5 font-mono text-xs font-normal">
 										{`${'0'.repeat(2 - (field.value.length + '').length) + field.value.length}/64 Chars`}
 									</FormDescription>
 									<FormMessage />
@@ -285,12 +294,12 @@ function TaskDialog({
 							control={form.control}
 							render={({ field }) => (
 								<FormItem>
-									<FormLabel className="mb-1 text-base font-medium">
+									<FormLabel className="text-base font-medium">
 										Description
 									</FormLabel>
-									<FormControl>
+									<FormControl className="!mt-0.5">
 										<Textarea
-											// defaultValue={out.description}
+											defaultValue={field.value}
 											placeholder="Task Description"
 											className="mb-4 h-auto resize-none"
 											{...field}
@@ -305,54 +314,55 @@ function TaskDialog({
 							control={form.control}
 							render={({ field }) => (
 								<FormItem>
-									<FormLabel className="mb-1 text-base font-medium">
+									<FormLabel className="text-base font-medium">
 										# of VPs
 									</FormLabel>
-									<FormControl>
+									<FormControl className="!mt-0.5">
 										<Input
-											// defaultValue={out.points}
+											defaultValue={field.value}
 											placeholder="Task VP Worth"
 											className="mb-4 w-[30%]"
 											type="number"
 											{...field}
+											onChange={(event) => field.onChange(+event.target.value)}
 										/>
 									</FormControl>
 									<FormMessage />
 								</FormItem>
 							)}
 						/>
-
 						<div className="grid grid-cols-2">
 							<FormField
 								name="type"
 								control={form.control}
 								render={({ field }) => (
 									<FormItem>
-										<FormLabel className="mb-1 text-base font-medium">
+										<FormLabel className="text-base font-medium">
 											Type
 										</FormLabel>
-										<FormControl className="mb-4 flex flex-row items-center justify-start gap-2">
+										<FormControl className="!mt-0.5 mb-4 flex flex-row items-center justify-start gap-2">
 											<ToggleGroup
 												type="single"
-												// defaultValue={out.type}
+												defaultValue={field.value}
 												variant={'outline'}
 												{...field}
-												className="mr-auto">
+												className="mr-auto"
+												onValueChange={(type) => field.onChange(type)}>
 												<ToggleGroupItem
 													value="single"
-													disabled={type == 'single'}
+													disabled={field.value == 'single'}
 													className="disabled:opacity-100">
 													Single
 												</ToggleGroupItem>
 												<ToggleGroupItem
 													value="daily"
-													disabled={type == 'daily'}
+													disabled={field.value == 'daily'}
 													className="disabled:opacity-100">
 													Daily
 												</ToggleGroupItem>
 												<ToggleGroupItem
 													value="multi"
-													disabled={type == 'multi'}
+													disabled={field.value == 'multi'}
 													className="disabled:opacity-100">
 													Multi
 												</ToggleGroupItem>
@@ -366,27 +376,28 @@ function TaskDialog({
 								control={form.control}
 								render={({ field }) => (
 									<FormItem>
-										<FormLabel className="mb-1 text-base font-medium">
+										<FormLabel className="text-base font-medium">
 											Priority
 											<span className="ml-2 text-xs font-light">
 												{'(not required if using single)'}
 											</span>
 										</FormLabel>
-										<FormControl className="mb-4 flex flex-row items-center justify-start gap-2">
+										<FormControl className="!mt-0.5 mb-4 flex flex-row items-center justify-start gap-2">
 											<ToggleGroup
 												type="single"
 												defaultValue={out.lower ? 'lower' : 'higher'}
 												variant={'outline'}
-												{...field}>
+												{...field}
+												onValueChange={(prio) => field.onChange(prio)}>
 												<ToggleGroupItem
 													value="higher"
-													disabled={!prio}
+													disabled={field.value == 'higher'}
 													className="disabled:opacity-100">
 													Higher
 												</ToggleGroupItem>
 												<ToggleGroupItem
 													value="lower"
-													disabled={prio}
+													disabled={field.value == 'lower'}
 													className="disabled:opacity-100">
 													Lower
 												</ToggleGroupItem>
@@ -408,7 +419,7 @@ function TaskDialog({
 									<Select
 										onValueChange={field.onChange}
 										defaultValue={field.value}>
-										<FormControl>
+										<FormControl className="!mt-0.5">
 											<SelectTrigger className="w-[40%]">
 												<SelectValue placeholder="Select a category" />
 											</SelectTrigger>
@@ -430,177 +441,15 @@ function TaskDialog({
 								</FormItem>
 							)}
 						/>
+						<DialogFooter>
+							<DialogClose>
+								<Button variant={'outline'}>Cancel</Button>
+							</DialogClose>
+
+							<Button type="submit">{task ? 'Submit' : 'Create'}</Button>
+						</DialogFooter>
 					</form>
 				</Form>
-				{/* <div className="h-full">
-					<Label className="mb-1 text-base font-medium" htmlFor="name">
-						Name
-					</Label>
-					<Input
-						id="name"
-						defaultValue={out.name}
-						placeholder="Task Name"
-						className="mb-4"
-						onChange={(evt) => {
-							const t = out;
-							t.name = evt.target.value;
-							setOut(t);
-							setName(evt.target.value);
-						}}
-						maxLength={64}
-					/>
-					<div className="-mt-3 mb-2 font-mono text-xs font-normal">
-						{`${'0'.repeat(2 - (name.length + '').length) + name.length}/64 Chars`}
-					</div>
-
-					<Label className="mb-1 text-base font-medium" htmlFor="desc">
-						Description
-					</Label>
-					<Textarea
-						id="desc"
-						defaultValue={out.description}
-						placeholder="Task Description"
-						className="mb-4 h-auto resize-none"
-						onChange={(evt) => {
-							const t = out;
-							t.description = evt.target.value;
-							setOut(t);
-						}}
-					/>
-
-					<Label className="mb-1 text-base font-medium" htmlFor="vp">
-						# of VPs
-					</Label>
-					<Input
-						id="vp"
-						defaultValue={out.points}
-						placeholder="Task VP Worth"
-						className="mb-4 w-[30%]"
-						type="number"
-						onChange={(evt) => {
-							const t = out;
-							t.points = +evt.target.value;
-							setOut(t);
-						}}
-					/>
-					<div className="grid grid-cols-2">
-						<div>
-							<Label className="mb-1 text-base font-medium" htmlFor="type">
-								Type
-							</Label>
-							<div className="mb-4 flex flex-row items-center gap-2" id="type">
-								<ToggleGroup
-									type="single"
-									defaultValue={out.type}
-									variant={'outline'}
-									onValueChange={(typ) => {
-										const t = out;
-										t.type = typ as Task['type'];
-										setOut(t);
-										setType(typ);
-									}}>
-									<ToggleGroupItem
-										value="single"
-										disabled={type == 'single'}
-										className="disabled:opacity-100">
-										Single
-									</ToggleGroupItem>
-									<ToggleGroupItem
-										value="daily"
-										disabled={type == 'daily'}
-										className="disabled:opacity-100">
-										Daily
-									</ToggleGroupItem>
-									<ToggleGroupItem
-										value="multi"
-										disabled={type == 'multi'}
-										className="disabled:opacity-100">
-										Multi
-									</ToggleGroupItem>
-								</ToggleGroup>
-							</div>
-						</div>
-						<div>
-							<Label className="mb-1 text-base font-medium" htmlFor="type">
-								Priority
-								<span className="ml-2 text-xs font-light">
-									{'(not required if using single)'}
-								</span>
-							</Label>
-							<div className="mb-4 flex flex-row items-center gap-2" id="type">
-								<ToggleGroup
-									type="single"
-									defaultValue={out.lower ? 'lower' : 'higher'} // FIXME: REQUIRE THERE TO BE ONE IN THE TOGGLE GROUP OTHERWISE IT WILL BE SCUFFED
-									variant={'outline'}
-									onValueChange={(lower) => {
-										const t = out;
-										t.lower = lower == 'lower';
-										setOut(t);
-										setPrio(lower == 'lower');
-									}}>
-									<ToggleGroupItem
-										value="higher"
-										disabled={!prio}
-										className="disabled:opacity-100">
-										Higher
-									</ToggleGroupItem>
-									<ToggleGroupItem
-										value="lower"
-										disabled={prio}
-										className="disabled:opacity-100">
-										Lower
-									</ToggleGroupItem>
-								</ToggleGroup>
-							</div>
-						</div>
-					</div>
-
-					<Label className="text-base font-medium" id="category">
-						Category
-					</Label>
-					<Select
-						defaultValue={out.category}
-						onValueChange={(category) => {
-							const t = out;
-							t.category = category as Task['category'];
-							setOut(t);
-						}}>
-						<SelectTrigger className="w-[40%]">
-							<SelectValue placeholder="Select a category" />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectGroup>
-								<SelectLabel>Category</SelectLabel>
-								<SelectItem value="health">Health</SelectItem>
-								<SelectItem value="normal">Become Normal</SelectItem>
-								<SelectItem value="cool">POV: Cool</SelectItem>
-								<SelectItem value="productivity">Productivity</SelectItem>
-								<SelectItem value="insane">INSANE</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
-				</div> */}
-				<DialogFooter>
-					<DialogClose>
-						<Button variant={'outline'}>Cancel</Button>
-					</DialogClose>
-					{/* {task ? (
-						<Button
-							onClick={() => {
-								edit();
-							}}>
-							Submit
-						</Button>
-					) : (
-						<Button
-							onClick={() => {
-								add();
-							}}>
-							Create
-						</Button>
-					)} */}
-					<Button type="submit">{task ? 'Submit' : 'Create'}</Button>
-				</DialogFooter>
 			</DialogContent>
 		</Dialog>
 	);
